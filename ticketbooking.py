3db465d90d8b49628ea4dfe90c4c01fb5cce77c0
Deekshitha Reddy <98227586+kdeekshithareddy@users.noreply.github.com>
2023-06-29 13:08:53 +0530

    ticketbooking.py

--- ticketbooking.py
+++ ticketbooking.py
@@ -20,3 +20,26 @@ class TicketBookingGUI:
             for j in range(10):
                 row.append(False)
             self.seats.append(row)
+
+   # GRID LAYOUT
+        for i in range(10):
+            for j in range(10):
+                seat_label = tk.Label(master, text="Seat {}:{}".format(i+1, j+1))
+                seat_button = tk.Button(master, text="Available", command=lambda i=i, j=j: self.select_seat(i, j))
+
+                seat_label.grid(row=i, column=j*2)
+                seat_button.grid(row=i, column=j*2+1)
+
+                self.seat_labels.append(seat_label)
+                self.seat_buttons.append(seat_button)
+
+        self.total_price_label.grid(row=10, column=0, columnspan=10)
+        self.book_button.grid(row=11, column=0, columnspan=10)
+
+    def select_seat(self, row, col):
+        if not self.seats[row][col]:
+            self.seats[row][col] = True
+            self.seat_buttons[row*10 + col]["text"] = "Selected"
+        else:
+            self.seats[row][col] = False
+            self.seat_buttons[row*10 + col]["text"] = "Available"
